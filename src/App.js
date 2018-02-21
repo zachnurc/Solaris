aaf09a9dfb988f2a4fa3cde1757d359a7ea51a33
zachnurc <zach.nurcombe@gmail.com>
2018-02-21 15:04:04 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,56 @@ import logo from './media/logo.png';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+  super(props);
+  this.state = {
+    scrollLocation: 0
+  };
+}
+
+handleKeyPress(keycode){
+
+  if(keycode === 40){
+    this.handleScroll('down');
+  }
+  else if(keycode === 38){
+    this.handleScroll('up');
+  }
+}
+
+//instead of scrolling load page with relevant #link
+
+handleScroll(direction){
+  if(direction === 'down'){
+    window.scroll(0, this.state.scrollLocation + window.innerHeight);
+  } else if(direction === 'up'){
+    window.scroll(0, this.state.scrollLocation - window.innerHeight);
+  }
+
+  this.setState({ scrollLocation: window.scrollY })
+
+}
+
+componentDidMount() {
+  //   document.addEventListener('scroll', this.handleScroll);
+  window.addEventListener("keydown", (e) => {
+    // space, page up, page down and arrow keys:
+    if([32, 33, 34, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+      e.preventDefault();
+      this.handleKeyPress(e.keyCode);
+    }
+  }, false);
+
+  // window.addEventListener("scroll", (event) => {
+  //   if (window.scrollY > this.state.scrollLocation){
+  //     this.handleScroll('down');
+  //   }else if(window.scrollY < this.state.scrollLocation){
+  //     this.handleScroll('up');
+  //   }
+  // }, false);
+}
+
   render() {
     return (
       <div className="App">
@@ -14,30 +64,28 @@ class App extends Component {
           <div id="navbar" >
             <ul>
               <li>
-                <a href="#meet-memo">Meet Memo</a>
+                <a href="#meet-memo">Meet Solaris</a>
               </li>
               <li>
-                <a href="#features">Features</a>
-              </li>
-              <li>
-                <a href="#connectivity">Connectivity</a>
-              </li>
-              <li>
-                <a href="#memo-app">Memo App</a>
-              </li>
-              <li>
-                <a href="#local-authorities">Local Authorities</a>
-              </li>
-              <li>
-                <a href="#contact">Contact Us</a>
+                <a href="#features">Contact</a>
               </li>
             </ul>
           </div>
 
         </header>
-        <p className="intro">
-          Hey DJ you suck
-        </p>
+        <div id="home">
+          <div className="intro">
+            <p>
+              Hey DJ you suck
+            </p>
+          </div>
+        </div>
+        <div id="contact">
+          <h3>Contact</h3>
+        </div>
+        <footer>
+          <p>Copyright Solaris 2017</p>
+        </footer>
       </div>
     );
   }
